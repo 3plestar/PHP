332b83c32f8bb8766952bef48bdb7574fd7f203e
David <David_dekker@live.nl>
2022-02-08 11:52:33 +0100

    h1

---
+++ C1-PHP Basis/H2/index.php
@@ -0,0 +1,37 @@
+<?php
+  $volgende_opdracht = "<br><br>";
+
+  //opdracht 1
+  $a = "Ik heb niet gefaald";
+  $b = "die niet werken";
+  $c = 10000;
+  echo $a.". Ik heb alleen ".$c." manieren gevonden ".$b.".<br><i>-Thomas Edison</i>";
+
+  echo $volgende_opdracht;
+
+  //opdracht 2
+  $a = "die geen fout";
+  $b = "en";
+  $c = "niet.";
+  echo "Mensen ".$a.$b.", werken ".$c."<br>
+  <i>
+    <a href='https://www.ikgastarten.nl/bedrijf-starten/de-start-van-klm' Target='blank'>
+      -Albert Plesman (KLM)
+    </a>
+  </i>";
+
+  echo $volgende_opdracht;
+
+  //opdracht 3
+  $a = "ry";
+  $b = ", but";
+  $c = "fail";
+  echo "T".$a." and ".$c.$b." never ".$c." to t".$a.".<br><i>-Tyra Banks</i>";
+
+  echo $volgende_opdracht;
+
+  //opdracht 4
+  $a = "\"omgaan";
+  $b = "met";
+  $c = "teleu";
+  echo "De cursus ".$a." ".$b." ".$c."rstellingen\" kan vanavond helaas niet doorgaan.<br><i>-Herman Finkers</i>";
